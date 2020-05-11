9482e4e7861054ba5212b095af9b1e4f09072fb5
Jayesh Sharma <37628273+jayesh24@users.noreply.github.com>
2020-05-11 23:27:45 +0530

    Updated

--- review-service/Review/src/test/java/com/cts/ReviewApplicationTests.java
+++ review-service/Review/src/test/java/com/cts/ReviewApplicationTests.java
@@ -6,7 +6,7 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.util.List;
+import java.util.List; 
 import java.util.stream.Collectors;
 import java.util.stream.Stream; 
 
