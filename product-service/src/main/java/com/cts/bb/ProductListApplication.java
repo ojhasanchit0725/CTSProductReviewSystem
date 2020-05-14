131a1757844ad7e8052e49e65e4f3c8df7c14590
jayesh24 <37628273+jayesh24@users.noreply.github.com>
2020-05-15 00:24:18 +0530

    final

--- product-service/src/main/java/com/cts/bb/ProductListApplication.java
+++ product-service/src/main/java/com/cts/bb/ProductListApplication.java
@@ -2,17 +2,19 @@ package com.cts.bb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
+@EnableCircuitBreaker
 @SpringBootApplication
 @EnableEurekaClient
 @EnableSwagger2
 @EnableDiscoveryClient
 @EnableHystrix
+
 public class ProductListApplication {  
  
 	public static void main(String[] args) {
